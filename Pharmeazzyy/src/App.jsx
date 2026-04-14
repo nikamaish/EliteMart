acd816b5c3cfb0fc37583f1b95e5cd900be98a8e
Aishwarya Nikam <aishnikam31@gmail.com>
2026-04-15 00:32:58 +0530

    :sparkles:basic structure of QuickActions action added

--- Pharmeazzyy/src/App.jsx
+++ Pharmeazzyy/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import QuickActions from './components/QuickActions'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <QuickActions/>
     </div>
   )
 }
---
+++ Pharmeazzyy/src/components/QuickActions.jsx
@@ -0,0 +1,19 @@
+import React, { useRef } from 'react'
+
+const QuickActions = () => {
+
+  return (
+   <section className="flex flex-column align-items-center justify-content-center gap-3" >
+    <div className='flex align-items-center justify-content-between w-full px-5 '>
+        <h3>What are you looking for?</h3>
+        <h3>Order with prescription. Upload Now</h3>
+
+    </div>
+    <div>
+       <h3>search bar</h3>
+    </div>
+   </section>
+  )
+}
+
+export default QuickActions
